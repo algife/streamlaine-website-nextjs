ada796f690e84429242bff37e14bb2248307538d
Alexandre G <algife@gmail.com>
2024-04-17 09:02:58 +0300

    Adding CallMeNow API Endpoint to app config

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 import type { LocalePrefix } from 'node_modules/next-intl/dist/types/src/shared/types';
 
-const localePrefix: LocalePrefix = 'as-needed';
+const localePrefix: LocalePrefix = 'always';
 
 // FIXME: Update this configuration file based on your project information
 export const AppConfig = {
@@ -8,4 +8,7 @@ export const AppConfig = {
   locales: ['en', 'es'],
   defaultLocale: 'en',
   localePrefix,
+  apiEndpoints: {
+    callMeNow: `${process.env.NEXT_PUBLIC_API_URL}/37a7208b-f334-4838-b43e-6ca977eae3c8`,
+  },
 };
