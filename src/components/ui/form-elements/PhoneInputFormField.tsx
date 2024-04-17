288e9a91948463a13aeeab238dd5090e78b154e4
Alexandre G <algife@gmail.com>
2024-04-17 09:03:44 +0300

    Allowing only US numbers in the CallMeNow form. Changing translation fields content.

--- src/components/ui/form-elements/PhoneInputFormField.tsx
+++ src/components/ui/form-elements/PhoneInputFormField.tsx
@@ -1,9 +1,22 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import 'react-phone-number-input/style.css';
 
+import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 import { useFormContext } from 'react-hook-form';
 import PhoneInput from 'react-phone-number-input';
+
+import cn from '@/utils/cn';
+
+function OnlyUSNumbersNote() {
+  return (
+    <div className="only-us-numbers-note">
+      <p>Only US numbers are allowed for now, but you can reach us in</p>
+      &nbsp;
+      <Link href="https://instagram.com/@streamlaine">our Instagram</Link>
+    </div>
+  );
+}
 
 export default function PhoneInputFormField({
   className,
@@ -11,7 +24,7 @@ export default function PhoneInputFormField({
   className?: string;
 }) {
   // const { register } = useFormContext();
-  const t = useTranslations('CallMeDialog');
+  const t = useTranslations('FormFields');
 
   const { setValue, watch } = useFormContext();
   const phoneValue = watch('phone');
@@ -38,7 +51,7 @@ export default function PhoneInputFormField({
   };
 
   return (
-    <div className="flex flex-auto flex-col justify-stretch">
+    <div className={cn('flex flex-auto flex-col justify-stretch', className)}>
       <label
         htmlFor="phone"
         className="hidden text-sm font-medium text-gray-500"
@@ -49,6 +62,7 @@ export default function PhoneInputFormField({
         addInternationalOption={false}
         smartCaret={false}
         defaultCountry="US"
+        countries={['US']}
         onChange={(newPhone) => handlePhoneUpdate({ phone: `${newPhone}` })}
         // onCountryChange={(countryCode) =>
         //   handlePhoneUpdate(undefined, countryCode)
@@ -56,8 +70,12 @@ export default function PhoneInputFormField({
         required
         value={phoneValue}
         placeholder={t('phone_placeholder')}
-        className="block w-full rounded-l-lg px-2 py-[0.7rem] text-sm text-gray-900 placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-0 "
+        className={cn(
+          'block w-full rounded-l-lg px-2 py-[0.7rem] text-sm',
+          'text-gray-900 placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-0',
+        )}
       />
+      <OnlyUSNumbersNote />
     </div>
   );
 }
